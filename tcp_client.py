367fbb8a5def2170730cc66fb8b502a29494032a
Andrei Sharafullin <sharafullin@gmail.com>
2020-05-11 14:48:35 +0100

    implemented discovery

--- tcp_client.py
+++ tcp_client.py
@@ -1,6 +1,7 @@
 import socket
 import sys
 import netifaces as ni
+from bluepy.btle import Scanner
 
 PORT = 35224
 ip = ni.ifaddresses('wlan0')[ni.AF_INET][0]['addr']
@@ -16,6 +17,8 @@ sock.bind(server_address)
 # Listen for incoming connections
 sock.listen(1)
 
+scanner = Scanner()
+
 while True:
     # Wait for a connection
     print("waiting for a connection")
@@ -27,10 +30,20 @@ while True:
         # Receive the data in small chunks and retransmit it
         while True:
             data = connection.recv(1024)
+            resp = ""
             print("received '%s'" % data)
             if data:
+                devices = scanner.scan(3.0)
+                for dev in devices:
+                    eq3 = False
+                    for (adtype, desc, value) in dev.getScanData():
+                        if desc == "Complete Local Name" and value == "CC-RT-BLE":
+                            eq3 = True
+                            break
+                    if eq3:
+                        resp += dev.addr + ":" + str(dev.rssi) + ";"
                 print("sending data back to the client")
-                connection.sendall(data)
+                connection.sendall(resp.encode())
             else:
                 print("no more data from", client_address)
                 break
